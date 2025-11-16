661ff358524cf231b852e1ad2b6fe13a7471273f
AiraYumi <aira.youme@airanyumi.net>
2025-11-16 21:36:17 +0900

    fix

--- indra/cmake/CURL.cmake
+++ indra/cmake/CURL.cmake
@@ -16,4 +16,8 @@ find_library(CURL_LIBRARY
 
 target_link_libraries(ll::libcurl INTERFACE ${CURL_LIBRARY} ll::openssl ll::nghttp2 ll::zlib-ng)
 
+if (DARWIN)
+    target_link_libraries(ll::libcurl INTERFACE "-framework SystemConfiguration")
+endif(DARWIN)
+
 target_include_directories( ll::libcurl SYSTEM INTERFACE ${LIBS_PREBUILT_DIR}/include)
